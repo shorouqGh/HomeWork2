628e68e1d5f25c6d1e044c2963948be5bcb67033
Shrooq <Shrooq@DESKTOP-UU9TB91>
2018-02-13 09:26:50 -0800

    helloo

--- src/Cal/test1.java
+++ src/Cal/test1.java
@@ -7,8 +7,8 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 public class test1 {
+	Model l =new Model();
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -29,6 +29,23 @@ public class test1 {
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	
+	assertEquals((int)2.0,(int)l.Div(8.0,4.0));
+	assertEquals(1,(int)(l.Div(8.0,0.0)));
+	assertEquals(1,(int)(l.mod(8.0,0.0)));
+	assertEquals(1,(int)(l.sqr(-1)));
+	
+	
+	
+	
+	
+	
+	
+	
 	}
-
+	
+	
+	
+	
+	
 }
