2b8938a3e19e94a00226474c46101c6a74d534b7
Shrooq <Shrooq@172.16.225.228>
2018-02-13 04:30:10 -0800

    k

--- src/Cal/test.java
+++ src/Cal/test.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.Assert.*;
-
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
 class Test {
 
 	Model l = new Model();
