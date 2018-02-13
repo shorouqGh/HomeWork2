4ea4c04297f44120565f0f1134c2f5d05eee539e
Shrooq <Shrooq@172.16.225.228>
2018-02-13 03:34:46 -0800

    HJ

--- src/Cal/test.java
+++ src/Cal/test.java
@@ -1,14 +1,45 @@
 package Cal;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.Assert.*;
 
-class test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+class Test {
+
+	Model l = new Model();
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
 	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		//fail("Not yet implemented");
+		
+		assertEquals((int)2.0,(int)l.Div(8.0,4.0));
+		assertEquals(1,(int)(l.Div(8.0,0.0)));
+		assertEquals(1,(int)(l.mod(8.0,0.0)));
+		assertEquals(1,(int)(l.sqr(-1)));
+		
+		
+		
+	}
+
+	
 
 }
